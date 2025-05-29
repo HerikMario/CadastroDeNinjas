4fff2c5bf35feff9551c8f73af2af5ea94c0f843
HerikMario <herikmariomr@gmail.com>
2025-05-29 01:18:11 -0300

    feat: Adiciona o MissaoRepository

---
+++ src/main/java/dev/java10x/CadastroNinja/Missoes/MissaoRepository.java
@@ -0,0 +1,9 @@
+package dev.java10x.CadastroNinja.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+
+
+
+}
