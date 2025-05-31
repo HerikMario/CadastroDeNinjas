69a2d25f0eef697470f1dba008c0934b13670bb3
HerikMario <herikmariomr@gmail.com>
2025-05-31 15:57:06 -0300

    chore: Adiciona o NinjaMapper para mapear minha entidade e meu dto

--- src/main/java/dev/java10x/CadastroNinja/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroNinja/Ninjas/NinjaMapper.java
@@ -1,4 +1,28 @@
 package dev.java10x.CadastroNinja.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+            return new NinjaModel(
+                    ninjaDTO.getId(),
+                    ninjaDTO.getNome(),
+                    ninjaDTO.getEmail(),
+                    ninjaDTO.getIdade(),
+                    ninjaDTO.getRank(),
+                    ninjaDTO.getMissao());
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        return new NinjaDTO(
+                ninjaModel.getId(),
+                ninjaModel.getNome(),
+                ninjaModel.getEmail(),
+                ninjaModel.getIdade(),
+                ninjaModel.getMissao(),
+                ninjaModel.getRank());
+    }
+
 }
